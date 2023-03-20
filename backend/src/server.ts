3b0b578ae11c95bc3a856934b367c5c4947aa41b
Leo Schneider <leocoelho256@gmail.com>
2023-03-20 18:20:10 -0300

    remove db.sync, and app running in PORT

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,10 @@
 import app from './app';
-import * as db from './database/models';
 
 const port = process.env.PORT || 3000;
 
-db.sequelize.sync().then(() => {
-  const server = app.listen(port, () => console.log(`app ouvindo na porta ${port}`));
-  
-  process.on('SIGINT', () => {
-    server.close();
-    console.log('App finalizado');
-  });
+const server = app.listen(port, () => console.log(`app ouvindo na porta ${port}`));
+
+process.on('SIGINT', () => {
+  server.close();
+  console.log('App finalizado');
 });
