79e35629ae73943babf939390743e3381ac196e3
Leo Schneider <leocoelho256@gmail.com>
2023-06-15 18:53:22 -0300

    configuring database

---
+++ backend_clothing_store/src/database/config/config.js
@@ -0,0 +1,23 @@
+const config = {
+  development: {
+    username: process.env.DATABASE_DEV_USER,
+    password: process.env.DATABASE_DEV_PASSWORD,
+    database: process.env.DATABASE_DEV_NAME,
+    host: process.env.DATABASE_DEV_HOST,
+    dialect: process.env.DATABASE_DEV_DIALECT
+  },
+  test: {
+    username: process.env.DATABASE_TEST_USER,
+    password: process.env.DATABASE_TEST_PASSWORD,
+    database: process.env.DATABASE_TEST_NAME,
+    host: process.env.DATABASE_TEST_HOST,
+    dialect: process.env.DATABASE_TEST_DIALECT
+  },
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT
+  }
+}
